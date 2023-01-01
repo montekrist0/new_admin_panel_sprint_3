6418b656eb05463085ac3325113fc8cda4a55a27
montekrist0 <bogdanovigor1994@mail.ru>
2023-01-01 21:21:25 +0300

    Add settings.py

---
+++ etl/settings.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+dsl = {
+    'dbname': os.environ.get('DB_NAME'),
+    'user': os.environ.get('DB_USER'),
+    'password': os.environ.get('DB_PASSWORD'),
+    'host': os.environ.get('DB_HOST'),
+    'port': int(os.environ.get('DB_PORT')),
+    'options': '-c search_path=content'
+}
